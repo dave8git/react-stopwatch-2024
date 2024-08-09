953cbcfdbaf9f8c2cfbadbe4c27ff332cf63732b
Dawid Polok <poldave111@gmail.com>
2024-08-09 16:30:25 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [time, setTime] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
+    const [intervalId, setIntervalId] = useState(null);
+
+    useEffect(() => {
+      if(isRunning) {
+        const id = setInterval(() => {
+          setTime(prevTime => prevTime + 1);
+        }, 1000);
+        setIntervalId(id);
+
+        return () => clearInterval(id)
+      } else {
+        if(intervalId) {
+          clearInterval(intervalId);
+        }
+      }
+
+      
+    }, [isRunning])
+    
+    const handleStart = () => {
+      setIsRunning(true);
+    }
+
+    const handleStop  = () => {
+      setIsRunning(false);
+    }
+
+    const handleClear = () => {
+      setIsRunning(false);
+      setTime(0);
+    }
+    
+    return (
+      <div className="button-container">
+        <button onClick={handleStart}>Start</button>
+        <button onClick={handleStop}>Stop</button>
+        <button onClick={handleClear}>Clear</button>
+        <div className="timer-display">
+            <h1>{time}s</h1>
+        </div>
+      </div>
+    );
+
 }
 
 export default App;
